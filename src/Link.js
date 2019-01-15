1af95d294e4e3bcbb5bfe0cb38b017715fddbab0
Dimitri Kopriwa <dka@dev-12.yeutech.vn>
2019-01-15 19:54:44 +0700

    feat(Link): Added <Link /> component
    Link that provide preloading capability (disabled by default) (through react-loadable)

---
+++ src/Link.js
@@ -0,0 +1,210 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import withRouter from 'react-router-dom/withRouter';
+import makeRoutes from './makeRoutes';
+
+/**
+ * @name Link
+ * @description
+ *
+ * This link component can preload a [react-loadable](https://github.com/jamiebuilds/react-loadable#loadablecomponentpreload) chunk on **mouseOver**
+ * It can also block the page change before the chunk is loaded,
+ * This give control on how the page change should work.
+ *
+ * @example
+ *
+ * Usually, you would create a `<Link />` component in your application `src/components/Link/index.js` as follow:
+ *
+ * ```javascript
+ * export default (
+ *   <Link
+ *    tag={A}
+ *    waitChunk={true}
+ *    // You can pass routes as props
+ *    routes={[{ name: 'Home', component: Home, path: '/' }]}
+ *    // OR a ContextConsumer that own it
+ *    ContextConsumer={AppContextConsumer}
+ *    onClick={actionStartLoadingIndicator}
+ *    onPreload={() => console.log(`
+ *      chunk load in the background on mouseover or
+ *      when user click if waitChunk is true
+ *    `)}
+ *    onLoaded={actionStopLoadingIndicator}
+ *   />
+ * );
+ * ```
+ *
+ */
+class Link extends React.Component {
+  static propTypes = {
+    /** the path the link will go to */
+    to: PropTypes.string.isRequired,
+    /** pass the component to be used for rendering */
+    tag: PropTypes.any,
+    /** event when click */
+    onClick: PropTypes.func,
+    /** event when page has changed */
+    onPageChange: PropTypes.func,
+    /** event when preloading start (react-loadable) */
+    onPreload: PropTypes.func,
+    /** event when preloading stop  (react-loadable) */
+    onLoaded: PropTypes.func,
+    /** event when mouse fly over */
+    onMouseOver: PropTypes.func,
+    /** define if react-router should change the page before or after the chunk is loaded */
+    waitChunk: PropTypes.bool,
+    /** the route list of the application, it supports childRoutes */
+    routes: PropTypes.array,
+    /**
+     * A context consumer that will provide routes from it's context,
+     * It also supports childRoutes.
+     */
+    ContextConsumer: PropTypes.any,
+    /** @ignore */
+    match: PropTypes.object.isRequired,
+    /** @ignore */
+    location: PropTypes.object.isRequired,
+    /** @ignore */
+    history: PropTypes.object.isRequired,
+  };
+
+  static defaultProps = {
+    tag: 'a',
+    waitChunk: false,
+    onClick: null,
+    onPageChange: null,
+    onPreload: null,
+    onLoaded: null,
+    onMouseOver: null,
+    routes: [],
+    ContextConsumer: null,
+  };
+
+  componentWillMount() {
+    const { routes, ContextConsumer } = this.props;
+    if (routes && ContextConsumer && process.env.NODE_ENV !== 'production') {
+      // eslint-disable-next-line no-console
+      console.warn(
+        'You passed routes and ContextConsumer props. You must use only one, ContextConsumer will be used and routes will be ignored.'
+      );
+    }
+  }
+
+  getComponent(path, routes) {
+    return makeRoutes(routes)
+      .filter((route) => route.props.path === path)[0]
+      .props.component;
+  }
+
+  onMouseOver = (e, routes) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const {
+      to,
+      onMouseOver,
+      onPreload,
+      onLoaded,
+    } = this.props;
+    const component = this.getComponent(to, routes);
+
+    if (onMouseOver) {
+      onMouseOver();
+    }
+    if (component.preload) {
+      if (onPreload) {
+        onPreload(e);
+      }
+      component.preload().then(() => {
+        if (onLoaded) {
+          onLoaded(e);
+        }
+      });
+    }
+  };
+
+  onClick = (e, routes) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const {
+      history,
+      to,
+      onClick,
+      onPageChange,
+      onPreload,
+      onLoaded,
+      waitChunk,
+    } = this.props;
+    const component = this.getComponent(to, routes);
+    if (onClick) {
+      onClick(e);
+    }
+    if (waitChunk && component.preload) {
+      if (onPreload) {
+        onPreload(e);
+      }
+      component.preload().then(() => {
+        if (onPageChange) {
+          onPageChange(e);
+        }
+        if (onLoaded) {
+          onLoaded(e);
+        }
+        history.push(to);
+      });
+      return;
+    }
+    if (onPageChange) {
+      onPageChange(e);
+    }
+    if (onLoaded) {
+      onLoaded(e);
+    }
+    history.push(to);
+  };
+
+  render() {
+    const {
+      tag: Tag,
+      to,
+      routes,
+      ContextConsumer,
+      // unused below
+      match,
+      location,
+      history,
+      onClick,
+      onPreload,
+      onPageChange,
+      onLoaded,
+      onMouseOver,
+      staticContext, // eslint-disable-line react/prop-types
+      waitChunk,
+      ...rest
+    } = this.props;
+    return ContextConsumer ? (
+      <ContextConsumer>
+        {({ routes: contextRoutes }) => (
+          <Tag
+            {...rest}
+            onClick={(e) => this.onClick(e, contextRoutes)}
+            onFocus={(e) => this.onMouseOver(e, contextRoutes)}
+            onMouseOver={(e) => this.onMouseOver(e, contextRoutes)}
+            href={to}
+            {...rest}
+          />
+        )}
+      </ContextConsumer>
+    ) : (
+      <Tag
+        {...rest}
+        onClick={(e) => this.onClick(e, routes)}
+        onFocus={(e) => this.onMouseOver(e, routes)}
+        onMouseOver={(e) => this.onMouseOver(e, routes)}
+        href={to}
+        {...rest}
+      />
+    );
+  }
+}
+
+export default withRouter(Link);
---
+++ src/tests/Link.test.js
@@ -0,0 +1,234 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { MemoryRouter } from 'react-router';
+import Link from '../Link';
+
+// this will mock the loadable component
+const LoadableComponent = () => <div />;
+LoadableComponent.preload = () => ({ then: (fn) => fn && fn() });
+
+const testRoutes = [
+  {
+    name: 'home',
+    path: '/',
+    exact: true,
+    component: LoadableComponent,
+  },
+  {
+    name: 'customers',
+    path: '/customers.html',
+    exact: true,
+    childRoutes: [
+      {
+        name: 'aboutUs',
+        path: '/about-us.html',
+        exact: true,
+      },
+      {
+        name: 'career',
+        path: '/career.html',
+        exact: true,
+      },
+      {
+        name: 'faq',
+        path: '/faq.html',
+        exact: true,
+      },
+      {
+        name: 'cgu',
+        path: '/CGU.html',
+        exact: true,
+      },
+      {
+        name: 'confidentiality',
+        path: '/Confidentiality.html',
+        exact: true,
+      },
+      {
+        name: 'cookies',
+        path: '/cookies.html',
+        exact: true,
+      },
+    ],
+  },
+  {
+    name: 'notFound',
+    path: '/404.html',
+    component: () => <div />,
+  },
+  {
+    name: 'notFoundRedirect',
+    from: '*',
+    to: '/404.html',
+  },
+];
+
+const ctx = React.createContext();
+const ContextProvider = (props) => <ctx.Provider {...props} value={{ routes: testRoutes }} />;
+const ContextConsumer = ctx.Consumer;
+
+
+const badCtx = React.createContext();
+const BadContextProvider = (props) => <badCtx.Provider {...props} value={{ routes: null }} />;
+const BadContextConsumer = badCtx.Consumer;
+
+const renderComponent = ({ to, ...rest }) => mount(
+  <MemoryRouter initialEntries={[to]}>
+    <ContextProvider>
+      <Link to={to} {...rest} />
+    </ContextProvider>
+  </MemoryRouter>
+);
+
+describe('<Link />', () => {
+  it('should shallow a <Link /> tag', () => {
+    const renderedComponent = shallow(<Link />);
+    expect(renderedComponent.length).toBe(1);
+  });
+  it('should mount a <Link /> tag', () => {
+    const renderedComponent = renderComponent({
+      routes: testRoutes,
+      to: '/404.html',
+    });
+    expect(renderedComponent.find('a').length).toBe(1);
+    expect(renderedComponent.find('a').props().href).toBe('/404.html');
+  });
+  it('should onMouseOver', () => {
+    const spy = jest.fn();
+    const renderedComponent = renderComponent({
+      routes: testRoutes,
+      to: '/404.html',
+      onMouseOver: spy,
+    });
+    renderedComponent.simulate('mouseover');
+    expect(spy).toHaveBeenCalled();
+  });
+  it('should onPreload and onLoaded onMouseOver', () => {
+    const spy = jest.fn();
+    const spyOnLoaded = jest.fn();
+    const renderedComponent = renderComponent({
+      routes: testRoutes,
+      to: '/',
+      onPreload: spy,
+      onLoaded: spyOnLoaded,
+    });
+    renderedComponent.simulate('mouseover');
+    expect(spy).toHaveBeenCalled();
+    expect(spyOnLoaded).toHaveBeenCalled();
+  });
+  it('should onMouseOver with preload', () => {
+    const renderedComponent = renderComponent({
+      routes: testRoutes,
+      to: '/',
+    });
+    renderedComponent.simulate('mouseover');
+    expect(renderedComponent.length).toBe(1);
+  });
+  it('should onLoaded onMouseOver', () => {
+    const spy = jest.fn();
+    const spyOnLoaded = jest.fn();
+    const renderedComponent = renderComponent({
+      routes: testRoutes,
+      to: '/',
+      onPreload: spy,
+      onLoaded: spyOnLoaded,
+    });
+    renderedComponent.simulate('mouseover');
+    expect(spy).toHaveBeenCalled();
+    expect(spyOnLoaded).toHaveBeenCalled();
+  });
+  it('should onClick onPageChange and onFocus', () => {
+    const spy = jest.fn();
+    const spyPageChange = jest.fn();
+    const spyFocus = jest.fn();
+    const renderedComponent = renderComponent({
+      routes: testRoutes,
+      to: '/404.html',
+      onClick: spy,
+      onPageChange: spyPageChange,
+      onFocus: spyFocus,
+    });
+    renderedComponent.simulate('click');
+    expect(spy).toHaveBeenCalled();
+    expect(spyPageChange).toHaveBeenCalled();
+    renderedComponent.find('a').prop('onFocus')();
+    expect(spyFocus).toHaveBeenCalled();
+  });
+  it('should onPageChange', () => {
+    const spy = jest.fn();
+    const renderedComponent = renderComponent({
+      routes: testRoutes,
+      to: '/404.html',
+      onPageChange: spy,
+    });
+    renderedComponent.simulate('click');
+    expect(spy).toHaveBeenCalled();
+  });
+  it('should onLoaded', () => {
+    const spy = jest.fn();
+    const renderedComponent = renderComponent({
+      routes: testRoutes,
+      to: '/404.html',
+      onLoaded: spy,
+    });
+    renderedComponent.simulate('click');
+    expect(spy).toHaveBeenCalled();
+  });
+  it('should waitChunk and onPreload onClick', () => {
+    const spy = jest.fn();
+    const spyPageChange = jest.fn();
+    const spyLoaded = jest.fn();
+    const renderedComponent = renderComponent({
+      routes: testRoutes,
+      to: '/',
+      onPreload: spy,
+      onPageChange: spyPageChange,
+      onLoaded: spyLoaded,
+      waitChunk: true,
+    });
+    renderedComponent.simulate('click');
+    expect(spy).toHaveBeenCalled();
+  });
+  it('should waitChunk onClick', () => {
+    const renderedComponent = renderComponent({
+      routes: testRoutes,
+      to: '/',
+      waitChunk: true,
+    });
+    renderedComponent.simulate('click');
+    expect(renderedComponent.length).toEqual(1);
+  });
+  it('should renderWith a ContextConsumer', () => {
+    const spy = jest.fn();
+    const spyPageChange = jest.fn();
+    const spyLoaded = jest.fn();
+    const spyFocus = jest.fn();
+    const spyMouseover = jest.fn();
+    const renderedComponent = renderComponent({
+      to: '/',
+      onPreload: spy,
+      onPageChange: spyPageChange,
+      onLoaded: spyLoaded,
+      onFocus: spyFocus,
+      onMouseOver: spyMouseover,
+      waitChunk: true,
+      ContextConsumer,
+    });
+    renderedComponent.find('a').simulate('click');
+    expect(spy).toHaveBeenCalled();
+    renderedComponent.find('a').prop('onFocus')();
+    expect(spyFocus).toHaveBeenCalled();
+    renderedComponent.find('a').simulate('mouseover');
+    expect(spyMouseover).toHaveBeenCalled();
+  });
+  it('should throw error due to bad ContextConsumer', () => {
+    const renderedComponent = mount(
+      <MemoryRouter initialEntries={['/']}>
+        <BadContextProvider>
+          <Link to="/" ContextConsumer={BadContextConsumer} />
+        </BadContextProvider>
+      </MemoryRouter>
+    );
+    expect(renderedComponent.length).toBe(1);
+  });
+});
