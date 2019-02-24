c372aa08b870727eb4139a3021bd900e3398b2ae
Dimitri Kopriwa <dka@dev-11.yeutech.vn>
2019-02-24 08:57:22 +0700

    fix(getPages): it now remove routes from the pages object

--- src/tests/getPages.test.js
+++ src/tests/getPages.test.js
@@ -30,75 +30,10 @@ describe('should exports', () => {
       }],
     }];
     const routesCopy = [...routes];
-    JSON.stringify(getPages(routes), null, 2);
     expect(getPages(routes)).toEqual({
-      bye: {
-        forest: {
-          gump: {
-            1337: {
-              name: '1337',
-              path: '/bye/forest/gump/1337',
-            },
-            name: 'gump',
-            path: '/bye/forest/gump',
-            routes: [
-              {
-                name: '1337',
-                path: '/bye/forest/gump/1337',
-              },
-            ],
-          },
-          name: 'forest',
-          path: '/bye/forest',
-          routes: [
-            {
-              name: 'gump',
-              path: '/bye/forest/gump',
-              routes: [
-                {
-                  name: '1337',
-                  path: '/bye/forest/gump/1337',
-                },
-              ],
-            },
-          ],
-        },
-        name: 'bye',
-        path: '/bye',
-        routes: [
-          {
-            name: 'forest',
-            path: '/bye/forest',
-            routes: [
-              {
-                name: 'gump',
-                path: '/bye/forest/gump',
-                routes: [
-                  {
-                    name: '1337',
-                    path: '/bye/forest/gump/1337',
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
       hello: {
         name: 'hello',
         path: '/hello',
-        routes: [
-          {
-            name: 'world',
-            path: '/hello/world',
-            routes: [
-              {
-                name: '2017',
-                path: '/hello/world/2017',
-              },
-            ],
-          },
-        ],
         world: {
           2017: {
             name: '2017',
@@ -106,12 +41,22 @@ describe('should exports', () => {
           },
           name: 'world',
           path: '/hello/world',
-          routes: [
-            {
-              name: '2017',
-              path: '/hello/world/2017',
+        },
+      },
+      bye: {
+        name: 'bye',
+        path: '/bye',
+        forest: {
+          name: 'forest',
+          path: '/bye/forest',
+          gump: {
+            1337: {
+              name: '1337',
+              path: '/bye/forest/gump/1337',
             },
-          ],
+            name: 'gump',
+            path: '/bye/forest/gump',
+          },
         },
       },
     });
